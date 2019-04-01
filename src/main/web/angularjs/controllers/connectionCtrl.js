7ead3ed2463ec10b573a1b95649115333b078340
jakubzol <jakubzol@student.agh.edu.pl>
2019-04-01 16:20:13 +0200

    Cleaning connection controller

--- src/main/web/angularjs/controllers/connectionCtrl.js
+++ src/main/web/angularjs/controllers/connectionCtrl.js
@@ -1,28 +1,6 @@
-app.controller("connectionCtrl", function($scope, $routeParams, timetablesService){
+app.controller("connectionCtrl", function($scope){
 
-   $scope.start = $routeParams.start.split("+").join(" ");
-   $scope.destination = $routeParams.destination.split("+").join(" ");
-   $scope.hour = parseInt($routeParams.hour);
-   $scope.minutes = parseInt($routeParams.minutes);
-   $scope.date = new Date($routeParams.date);
-   $scope.lines = $routeParams.lines.split("+");
-   $scope.currentDate = new Date();
 
-   $scope.params = {
-        lines: $scope.lines,
-        start: $scope.start,
-        destination: $scope.destination,
-        date: $scope.date,
-        hour: $scope.hour,
-        minutes: $scope.minutes
-    };
-
-   timetablesService.searchConnections($scope.params).then(function(data){
-      $scope.connections = data;
-      console.log(data);
-   }).catch(function(error){
-       console.log(error);
-   });
 
 
 });
